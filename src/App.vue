397f81f66673059359b6b021570d64da017a17f4
wangshiyuan <wangshiyuan@zhito.com>
2023-07-21 15:10:55 +0800

    grid layout

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-    <div class="border col-span-2">header</div>
-    <div class="border">left</div>
-    <div class="border">right</div>
+    <div class="border col-span-2"></div>
+    <div class="border"></div>
+    <div class="border p-1"><div class="h-full border">
+    Lorem ipsum dolor, sit amet consectetur <span class="font-bold inline-block">adipisicingelit. Eaque hic odit ipsa, culpa in at?</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    </div></div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    darkMode: true,
+    darkMode: "class",
     theme: {
         extend: {}
     },
